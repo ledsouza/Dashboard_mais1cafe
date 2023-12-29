f9ea37fcc7d2b5167ad6e6bd790b62084477dd3d
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2023-12-29 15:37:29 -0300

    Formatada a tabela

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,66 @@
+import yaml
+from yaml.loader import SafeLoader
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import streamlit_authenticator as stauth
+import pandas as pd
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+# --- USER AUTHENTICATION ---
+with open(".streamlit/config.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-data = conn.read(
-    usecols=range(6),
-    ttl='1m'
-)
+    authenticator = stauth.Authenticate(
+        config["credentials"],
+        config["cookie"]["name"],
+        config["cookie"]["key"],
+        config["cookie"]["expiry_days"],
+    )
 
-updated_data = st.data_editor(data)
+name, authentication_status, username = authenticator.login("Login", "main")
 
-if not data.equals(updated_data):
-    conn.update(data=updated_data)
+if authentication_status is False:
+    st.error("Usuário ou senha incorreto")
+
+if authentication_status is None:
+    st.warning("Por favor, insira o usuário e a senha")
+
+if authentication_status:
+    authenticator.logout("Logout", "sidebar")
+
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    data = conn.read(usecols=range(6), ttl="1m")
+    data['Data'] = pd.to_datetime(data['Data'], format='%m/%d/%Y')
+    data[['Clientes', 'PA', 'Ticket Médio', 'Faturamento']] = data[['Clientes', 'PA', 'Ticket Médio', 'Faturamento']] * 100
+
+    updated_data = st.data_editor(
+        data,
+        column_config={
+            'Data': st.column_config.DatetimeColumn(
+                'Data',
+                format='DD.MM.YYYY'
+            ),
+            'Clientes': st.column_config.NumberColumn(
+                'Clientes',
+                format='%d%%'
+            ),
+            'PA': st.column_config.NumberColumn(
+                'PA',
+                format='%d%%'
+            ),
+            'Ticket Médio': st.column_config.NumberColumn(
+                'Ticket Médio',
+                format='%d%%'
+            ),
+            'Faturamento': st.column_config.NumberColumn(
+                'Faturamento',
+                format='%d%%'
+            )
+        }
+    )
+
+    if not data.equals(updated_data):
+        updated_data[['Clientes', 'PA', 'Ticket Médio', 'Faturamento']] = updated_data[['Clientes', 'PA', 'Ticket Médio', 'Faturamento']] / 100
+        conn.update(data=updated_data)
