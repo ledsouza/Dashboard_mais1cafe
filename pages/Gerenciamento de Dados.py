201ccb0d85f85a6016aeb958a2e29173daeb51db
Leandro Souza <leandro.souza.159@gmail.com>
2024-01-27 14:07:00 -0300

    get_user_input refactored

--- pages/Gerenciamento de Dados.py
+++ pages/Gerenciamento de Dados.py
@@ -14,34 +14,25 @@ def read_data(conn):
     data.sort_values(by="Data", ascending=False, inplace=True)
     return data
 
-def get_user_input(metas_max_value, pa_max_value):
-    date = pd.to_datetime(st.date_input(label='Data'))
-    clientes = st.number_input(label='Clientes', max_value=metas_max_value)
-    produtos = st.number_input(label='Produtos', max_value=metas_max_value)
-    pa = st.number_input(label='PA', max_value=pa_max_value)
-    ticket_medio = st.number_input(label='Ticket Médio', max_value=metas_max_value)
-    faturamento = st.number_input(label='Faturamento', max_value=metas_max_value)
-    cliente_hora = st.number_input(label='Cliente/Hora')
-    clima = st.selectbox(label='Clima', options=['Ensolarado', 'Nublado', 'Chuvoso', 'Tempestade', 'Vendaval'])
+def get_user_input(conn, metas_max_value, pa_max_value, selected_metas=['Clientes', 'Produtos', 'PA', 'Ticket Médio', 'Faturamento', 'Cliente/Hora', 'Clima']):
+    metas = {}
+    data = read_data(conn)
     
-    return date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima
+    metas = {
+        'Data': pd.to_datetime(st.date_input(label='Data')),
+        'Clientes': st.number_input(label='Clientes', max_value=metas_max_value) if 'Clientes' in selected_metas else data.query('Data == @metas["Data"]')['Clientes'].values[0],
+        'Produtos': st.number_input(label='Produtos', max_value=metas_max_value) if 'Produtos' in selected_metas else data.query('Data == @metas["Data"]')['Produtos'].values[0],
+        'PA': st.number_input(label='PA', max_value=pa_max_value) if 'PA' in selected_metas else data.query('Data == @metas["Data"]')['PA'].values[0],
+        'Ticket Médio': st.number_input(label='Ticket Médio', max_value=metas_max_value) if 'Ticket Médio' in selected_metas else data.query('Data == @metas["Data"]')['Ticket Médio'].values[0],
+        'Faturamento': st.number_input(label='Faturamento', max_value=metas_max_value) if 'Faturamento' in selected_metas else data.query('Data == @metas["Data"]')['Faturamento'].values[0],
+        'Cliente/Hora': st.number_input(label='Cliente/Hora') if 'Cliente/Hora' in selected_metas else data.query('Data == @metas["Data"]')['Cliente/Hora'].values[0],
+        'Clima': st.selectbox(label='Clima', options=['Ensolarado', 'Nublado', 'Chuvoso', 'Tempestade', 'Vendaval']) if 'Clima' in selected_metas else data.query('Data == @metas["Data"]')['Clima'].values[0]
+    }
+    
+    return metas
 
-def update_data(conn, data, date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima):
-    updated_data = pd.DataFrame(
-        [
-            {
-                "Data": date,
-                "Clientes": clientes,
-                "Produtos": produtos,
-                "PA": pa,
-                "Ticket Médio": ticket_medio,
-                "Faturamento": faturamento,
-                "Cliente/Hora": cliente_hora,
-                "Clima": clima
-            }
-        ]
-    )
-
+def update_data(conn, data, metas):
+    updated_data = pd.DataFrame([metas])
     updated_df = pd.concat([data, updated_data], ignore_index=True)
     conn.update(data=updated_df)
 
@@ -77,29 +68,29 @@ if authentication_status:
     pa_max_value = 4.0    
     with insert_tab:
         with st.form(key="insert_data", clear_on_submit=True):
-            date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima = get_user_input(metas_max_value, pa_max_value)
+            metas = get_user_input(conn, metas_max_value, pa_max_value)
 
             submit_button = st.form_submit_button(label="Inserir dados")
             if submit_button:
                 data = read_data(conn)
-                if not data.query('Data == @date').empty:
+                if not data.query('Data == @metas["Data"]').empty:
                     st.error("Dados já existentes para a data inserida")
                 else:                    
-                    update_data(conn, data, date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima)
+                    update_data(conn, data, metas)
                     st.success("YESSSSS")   
 
     with update_tab:
         with st.form(key="update_data", clear_on_submit=True):
-            date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima = get_user_input(metas_max_value, pa_max_value)
+            metas = get_user_input(conn, metas_max_value, pa_max_value)
 
             submit_button = st.form_submit_button(label="Atualizar dados")
             if submit_button:
                 data = read_data(conn)
-                if data.query('Data == @date').empty:
+                if data.query('Data == @metas["Data"]').empty:
                     st.error("Data não encontrada")
                 else:
-                    data = data[data['Data'] != date] # Removendo a data para atualizar
-                    update_data(conn, data, date, clientes, produtos, pa, ticket_medio, faturamento, cliente_hora, clima)
+                    data = data[data['Data'] != metas['Data']] # Removendo a data para atualizar
+                    update_data(conn, data, metas)
                     st.success("YESSSSS")
 
     with delete_tab:
