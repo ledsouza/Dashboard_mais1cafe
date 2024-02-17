2944664066a69be89dc6ed75c1bb42b9f041afe5
Leandro Souza <leandro.souza.159@gmail.com>
2024-02-17 18:56:13 -0300

    Construído os formulários

--- pages/Gerenciamento de Dados.py
+++ pages/Gerenciamento de Dados.py
@@ -5,70 +5,135 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 from connection import mongo_connection
+import time
+
+st.set_page_config(layout="wide")
 
 # Conexão com o banco de dados
 client = mongo_connection()
 db = client["db_mais1cafe"]
 collection = db["metas"]
 
-# def read_data(conn):
-#     data = conn.read(usecols=range(8), ttl="0")
-#     data.dropna(inplace=True)
-#     data["Data"] = pd.to_datetime(data["Data"], format="%m/%d/%Y")
-#     data.sort_values(by="Data", ascending=False, inplace=True)
-#     return data
 
-# def get_user_input(conn, metas_max_value, pa_max_value, selected_metas=['Clientes', 'Produtos', 'PA', 'Ticket Médio', 'Faturamento', 'Cliente/Hora', 'Clima']):
-#     metas = {}
-#     data = read_data(conn)
-#     metas['Data'] = pd.to_datetime(st.date_input(label='Data'))
-#     if data.query('Data == @metas["Data"]').empty and selected_metas is None: 
-#         st.error("Dados não existentes para a data inserida")
-#         st.stop()
+def get_user_input(
+    metas_max_value,
+    pa_max_value,
+    selected_metas=[
+        "Clientes",
+        "Produtos",
+        "PA",
+        "Ticket Médio",
+        "Faturamento",
+        "Cliente/Hora",
+        "Clima",
+    ],
+):
+    metas = {}
 
-#     if 'Clientes' in selected_metas:
-#         metas['Clientes'] = st.number_input(label='Clientes', max_value=metas_max_value)
-#     else:
-#         metas['Clientes'] = data.query('Data == @metas["Data"]')['Clientes'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    metas["Data"] = pd.to_datetime(st.date_input(label="Data"))
 
-#     if 'Produtos' in selected_metas:
-#         metas['Produtos'] = st.number_input(label='Produtos', max_value=metas_max_value)
-#     else:
-#         metas['Produtos'] = data.query('Data == @metas["Data"]')['Produtos'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "Clientes" in selected_metas:
+        metas["Clientes"] = st.number_input(label="Clientes", max_value=metas_max_value)
 
-#     if 'PA' in selected_metas:
-#         metas['PA'] = st.number_input(label='PA', max_value=pa_max_value)
-#     else:
-#         metas['PA'] = data.query('Data == @metas["Data"]')['PA'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "Produtos" in selected_metas:
+        metas["Produtos"] = st.number_input(label="Produtos", max_value=metas_max_value)
 
-#     if 'Ticket Médio' in selected_metas:
-#         metas['Ticket Médio'] = st.number_input(label='Ticket Médio', max_value=metas_max_value)
-#     else:
-#         metas['Ticket Médio'] = data.query('Data == @metas["Data"]')['Ticket Médio'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "PA" in selected_metas:
+        metas["PA"] = st.number_input(label="PA", max_value=pa_max_value)
 
-#     if 'Faturamento' in selected_metas:
-#         metas['Faturamento'] = st.number_input(label='Faturamento', max_value=metas_max_value)
-#     else:
-#         metas['Faturamento'] = data.query('Data == @metas["Data"]')['Faturamento'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "Ticket Médio" in selected_metas:
+        metas["Ticket Médio"] = st.number_input(
+            label="Ticket Médio", max_value=metas_max_value
+        )
 
-#     if 'Cliente/Hora' in selected_metas:
-#         metas['Cliente/Hora'] = st.number_input(label='Cliente/Hora')
-#     else:
-#         metas['Cliente/Hora'] = data.query('Data == @metas["Data"]')['Cliente/Hora'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "Faturamento" in selected_metas:
+        metas["Faturamento"] = st.number_input(
+            label="Faturamento", max_value=metas_max_value
+        )
 
-#     if 'Clima' in selected_metas:
-#         metas['Clima'] = st.selectbox(label='Clima', options=['Ensolarado', 'Nublado', 'Chuvoso', 'Tempestade', 'Vendaval'])
-#     else:
-#         metas['Clima'] = data.query('Data == @metas["Data"]')['Clima'].values[0] if not data.query('Data == @metas["Data"]').empty else None
+    if "Cliente/Hora" in selected_metas:
+        metas["Cliente/Hora"] = st.number_input(label="Cliente/Hora")
 
-#     return metas
+    if "Clima" in selected_metas:
+        metas["Clima"] = st.selectbox(
+            label="Clima",
+            options=["Ensolarado", "Nublado", "Chuvoso", "Tempestade", "Vendaval"],
+        )
 
-# def update_data(conn, data, metas):
-#     updated_data = pd.DataFrame([metas])
-#     updated_df = pd.concat([data, updated_data], ignore_index=True)
-#     conn.update(data=updated_df)
+    return metas
 
-# st.set_page_config(layout="wide")
+
+def update_meta(metas):
+    query = {"Data": metas["Data"]}
+    update = {"$set": metas}
+    update_status = collection.update_one(query, update)
+    return update_status.acknowledged
+
+
+metas_max_value = 3.0
+pa_max_value = 4.0
+
+
+insert_tab, update_tab, delete_tab, db_tab = st.tabs(
+    ["Inserir", "Atualizar", "Deletar", "Banco de Dados"]
+)
+
+with insert_tab:
+    with st.form(key="insert_data", clear_on_submit=True):
+        metas = get_user_input(metas_max_value, pa_max_value)
+        submit_button = st.form_submit_button(label="Inserir dados")
+        if submit_button:
+            insert_status = collection.insert_one(metas)
+            if insert_status.acknowledged:
+                st.success("YESSSSS")
+            else:
+                st.error("Erro ao inserir os dados")
+
+with update_tab:
+    selected_metas = st.multiselect(
+        label="Metas",
+        options=[
+            "Clientes",
+            "Produtos",
+            "PA",
+            "Ticket Médio",
+            "Faturamento",
+            "Cliente/Hora",
+            "Clima",
+        ],
+        placeholder="Selecione as metas que deseja atualizar",
+    )
+    if not selected_metas:
+        st.error("É necessário selecionar pelo menos uma meta")
+    else:
+        with st.form(key="update_data", clear_on_submit=True):
+            metas = get_user_input(metas_max_value, pa_max_value, selected_metas)
+            submit_button = st.form_submit_button(label="Atualizar dados")
+            if submit_button:
+                update_status = update_meta(metas)
+                if update_status:
+                    st.success("YESSSSS")
+                else:
+                    st.error("Erro ao atualizar os dados")
+
+with delete_tab:
+    with st.form(key="delete_data", clear_on_submit=True):
+        date = pd.to_datetime(st.date_input(label="Data"))
+        submit_button = st.form_submit_button(label="Deletar dados")
+        if submit_button:
+            delete_status = collection.delete_one({"Data": date})
+            if delete_status.acknowledged:
+                st.success("YESSSSS")
+            else:
+                st.error("Erro ao deletar os dados")
+
+with db_tab:
+    metas_df = pd.DataFrame(collection.find({}, {"_id": 0})).sort_values(
+        by="Data", ascending=False
+    )
+    st.dataframe(metas_df, hide_index=True, use_container_width=True)
+
+#
 
 # # --- USER AUTHENTICATION ---
 # with open(".streamlit/config.yaml") as file:
@@ -95,9 +160,9 @@ collection = db["metas"]
 #     conn = st.connection("gsheets", type=GSheetsConnection)
 
 #     insert_tab, update_tab, delete_tab = st.tabs(['Inserir', 'Atualizar', 'Deletar'])
-    
+
 #     metas_max_value = 3.0
-#     pa_max_value = 4.0    
+#     pa_max_value = 4.0
 #     with insert_tab:
 #         with st.form(key="insert_data", clear_on_submit=True):
 #             metas = get_user_input(conn, metas_max_value, pa_max_value)
@@ -107,9 +172,9 @@ collection = db["metas"]
 #                 data = read_data(conn)
 #                 if not data.query('Data == @metas["Data"]').empty:
 #                     st.error("Dados já existentes para a data inserida")
-#                 else:                    
+#                 else:
 #                     update_data(conn, data, metas)
-#                     st.success("YESSSSS")   
+#                     st.success("YESSSSS")
 
 #     with update_tab:
 #         selected_metas = st.multiselect(label='Metas', options=['Clientes', 'Produtos', 'PA', 'Ticket Médio', 'Faturamento', 'Cliente/Hora', 'Clima'], placeholder='Selecione as metas que deseja atualizar')
@@ -132,7 +197,7 @@ collection = db["metas"]
 #     with delete_tab:
 #         with st.form(key="delete_data", clear_on_submit=True):
 #             date = pd.to_datetime(st.date_input(label='Data'))
-            
+
 #             submit_button = st.form_submit_button(label="Deletar dados")
 #             if submit_button:
 #                 data = read_data(conn)
@@ -143,7 +208,7 @@ collection = db["metas"]
 #                     conn.update(data=data)
 
 #                     st.success("YESSSSS")
-    
+
 #     # Banco de dados completa
 #     data = read_data(conn)
 
