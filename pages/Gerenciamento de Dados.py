9563f210f07fa4b70ab710549dcfc91f25607c20
Leandro Souza <leandro.souza.159@gmail.com>
2024-03-02 18:38:57 -0300

    Add user authentication

--- pages/Gerenciamento de Dados.py
+++ pages/Gerenciamento de Dados.py
@@ -1,15 +1,26 @@
 import streamlit as st
-import pandas as pd
+from Modules.user_authentication import create_authenticator
 from Modules.connection import database_connection
 from Modules.forms import FormMetas
 
 st.set_page_config(layout="wide")
 
-collection = database_connection("metas")
+authenticator = create_authenticator()
+name, authentication_status, username = authenticator.login("Login", "main")
 
-form_metas = FormMetas(collection)
+if authentication_status is False:
+    st.error("Usuário ou senha incorreto")
 
-form_metas.create_insert_form()
-form_metas.create_update_form()
-form_metas.create_delete_form()
-form_metas.create_database_tab()
+if authentication_status is None:
+    st.warning("Por favor, insira o usuário e a senha")
+
+if authentication_status:
+    authenticator.logout("Logout", "sidebar")
+    collection = database_connection("metas")
+
+    form_metas = FormMetas(collection)
+
+    form_metas.create_insert_form()
+    form_metas.create_update_form()
+    form_metas.create_delete_form()
+    form_metas.create_database_tab()
