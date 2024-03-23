85afeba99ce468ddbd14d4ae2724bd97c1a4d3aa
Leandro Souza <leandro.souza.159@gmail.com>
2024-03-23 15:33:28 -0300

    refactored conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
+from unittest.mock import MagicMock
 import pytest
 import streamlit as st
 import pymongo
-from unittest.mock import MagicMock
 
 @pytest.fixture(scope="session")
 def mongodb():
+    """
+    Connects to a MongoDB database using the provided credentials and returns the client object.
+
+    Returns:
+        pymongo.MongoClient: The MongoDB client object.
+
+    Raises:
+        AssertionError: If the connection to the database fails.
+    """
     mongodb_password = st.secrets['db_credential']['password']
     uri = f"mongodb+srv://ledsouza:{mongodb_password}@cluster-mais1cafe.editxaq.mongodb.net/?retryWrites=true&w=majority"
 
@@ -14,12 +23,31 @@ def mongodb():
 
 @pytest.fixture
 def mock_mongodb():
-    mock_mongodb = MagicMock()
-    mock_mongodb.insert_meta.side_effect = Exception("Mock Exception")
-    return mock_mongodb
+    """
+    Fixture for mocking the MongoDB connection.
+
+    This fixture creates a MagicMock object that simulates a MongoDB connection.
+    It also sets the side effect of the mock object to raise an Exception with the message "Mock Exception".
+
+    Returns:
+        MagicMock: The mocked MongoDB connection object.
+    """
+    mock_client = MagicMock()
+    mock_client.side_effect = Exception("Mock Exception")
+    return mock_client
 
 @pytest.fixture
 def rollback_session(mongodb):
+    """
+    Fixture that provides a rollback session for MongoDB transactions.
+
+    This fixture starts a MongoDB session and begins a transaction. It yields the session
+    object, allowing the test function to use it. After the test function completes, the
+    transaction is aborted, ensuring that any changes made during the test are rolled back.
+
+    Yields:
+        pymongo.client.Session: The MongoDB session object.
+    """
     session = mongodb.start_session()
     session.start_transaction()
     try:
