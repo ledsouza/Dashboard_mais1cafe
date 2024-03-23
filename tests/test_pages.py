b1b4b130b00369d96709e67c097d9d7445b302e1
Leandro Souza <leandro.souza.159@gmail.com>
2024-03-23 19:14:47 -0300

    added test_home_valid_password

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from streamlit.testing.v1 import AppTest
 
 # TODO: Add tests for the pages.
@@ -21,4 +22,19 @@ def test_home_invalid_password():
     at.button[0].click()
     at.run()
     assert at.session_state["authentication_status"] is False
+    assert not at.exception
+
+def test_home_valid_password():
+    """
+    Test case to verify the behavior of the home page when a valid password is entered.
+    """
+    at = AppTest.from_file("home.py")
+    at.run()
+    user = st.secrets['login_credential']['user']
+    password = st.secrets['login_credential']['password']
+    at.text_input[0].set_value(user)
+    at.text_input[1].set_value(password)
+    at.button[0].click()
+    at.run()
+    assert at.session_state["authentication_status"] is True
     assert not at.exception
